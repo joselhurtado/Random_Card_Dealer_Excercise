ab754b83bbf6e1e4bc3927f59657a075bf878732
Jose Hurtado <hello@josehurtado.net>
2021-01-16 01:59:49 +0000

    New files per NPM Issues on previous task

--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  function generateRandomSuit() {
+    let suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+    return suits[Math.floor(Math.random() * suits.length)];
+  }
+
+  function generateRandomNumber() {
+    let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    {
+      return numbers[Math.floor(Math.random() * numbers.length)];
+    }
+  }
+  var design = generateRandomSuit();
+
+  if (design == "&hearts;" || design == "&diams;") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".numbers").style.color = "red";
+  } else if (design == "&spades;" || design == "&clubs;") {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+    document.querySelector(".numbers").style.color = "black";
+  }
+
+  document.querySelector(".top-suit").innerHTML = design;
+  document.querySelector(".bottom-suit").innerHTML = design;
+  document.querySelector(".numbers").innerHTML = generateRandomNumber();
 };
